07a411cbe9c45eefdd8e80ce333e718faf45ef82
sakaguchi <kzmsan63@gmail.com>
2024-02-25 22:00:04 +0900

    Header修正

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -19,7 +19,7 @@ export function Header() {
     <header className="bg-zinc-50">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex items-center lg:flex-1">
-          <a href="#" className="flex items-center -m-1.5 p-1.5">
+          <a href="/pomodoro" className="flex items-center -m-1.5 p-1.5">
             <img className="h-10 w-auto mr-2" src="src/assets/logo.png" alt="Pomodoro Logo" />
             <span className="text-lg font-medium">Pomodoro</span>
           </a>
@@ -41,8 +41,8 @@ export function Header() {
           <a href="/todo" className="text-sm font-semibold leading-6 text-gray-900">
             Todo
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Company
+          <a href="/report" className="text-sm font-semibold leading-6 text-gray-900">
+            Report
           </a>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -55,13 +55,9 @@ export function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+            <a href="/pomodoro" className="flex items-center -m-1.5 p-1.5">
+              <img className="h-10 w-auto mr-2" src="src/assets/logo.png" alt="Pomodoro Logo" />
+              <span className="text-lg font-medium">Pomodoro</span>
             </a>
             <button
               type="button"
@@ -76,22 +72,22 @@ export function Header() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 <a
-                  href="#"
+                  href="/pomodoro"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Features
+                  Pomodoro
                 </a>
                 <a
-                  href="#"
+                  href="/todo"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Marketplace
+                  Todo
                 </a>
                 <a
-                  href="#"
+                  href="/report"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Company
+                  Report
                 </a>
               </div>
               <div className="py-6">
