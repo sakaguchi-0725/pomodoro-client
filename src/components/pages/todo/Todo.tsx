8b6a142684ee029194afe0200cbebccd15bea7dd
sakaguchi <kzmsan63@gmail.com>
2024-02-26 22:20:01 +0900

    Todo画面修正

--- src/components/pages/todo/Todo.tsx
+++ src/components/pages/todo/Todo.tsx
@@ -1,59 +1,37 @@
-import useStore from "../../../store/task"
 import { useQueryTasks } from "../../../hooks/useQueryTasks"
-import { useMutateTask } from "../../../hooks/useMutateTask"
-import { FormEvent } from "react"
-import { ShieldCheckIcon } from "@heroicons/react/24/outline"
-import { TaskItem } from "./components/TaskItem"
+import { useRef, useState } from "react"
+import { TaskItem } from "./components/TodoItem"
+import { Card } from "../../common/Card"
+import { Modal } from "../../common/Modal"
+import { TodoModal } from "./components/TodoModal"
 
 export const Todo = () => {
-  const { editedTask } = useStore()
-  const updateTask = useStore((state) => state.updateEditedTask)
+  const [open, setOpen] = useState(false)
+  const cancelButtonRef = useRef(null)
   const { data, isLoading } = useQueryTasks()
-  const { createTaskMutation, updateTaskMutation } = useMutateTask()
-
-  const submitTaskHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    if (editedTask.id === 0) {
-      createTaskMutation.mutate({
-        title: editedTask.title
-      })
-    } else {
-      updateTaskMutation.mutate(editedTask)
-    }
-  }
 
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
-      <div className="flex items-center my-3">
-        <ShieldCheckIcon className="h-8 w-8 mr-3 text-indigo-500 cursor-pointer" />
-        <span className="text-center text-3xl font-extrabold">
-          Task Manager
-        </span>
-      </div>
-      <form onSubmit={submitTaskHandler}>
-        <input
-          className="mb-3 mr-3 px-3 py-2 border border-gray-300"
-          placeholder="title ?"
-          type="text"
-          onChange={(e) => updateTask({ ...editedTask, title: e.target.value })}
-          value={editedTask.title || ''}
-        />
+    <>
+      <Card>
         <button
-          className="disabled:opacity-40 mx-3 py-2 px-3 text-white bg-indigo-600 rounded"
-          disabled={!editedTask.title}
+          className="border w-full h-12 rounded mb-2 border-slate-300 bg-slate-100 font-semibold text-slate-600 hover:bg-slate-200"
+          onClick={() => setOpen(true)}
         >
-          {editedTask.id === 0 ? 'Create' : 'Update'}
+          Add Todo
         </button>
-      </form>
-      {isLoading ? (
-        <p>Loading..</p>
-      ) : (
-        <ul className="my-5">
-          {data?.map((task) => (
-            <TaskItem key={task.id} id={task.id} title={task.title} />
-          ))}
-        </ul>
-      )}
-    </div>
+        {isLoading ? (
+          <p>Loading..</p>
+        ) : (
+          <ul role="list" className="divide-y divide-gray-100">
+            {data?.map((task) => (
+              <TaskItem key={task.id} task={task} setOpen={setOpen} />
+            ))}
+          </ul>
+        )}
+      </Card>
+      <Modal open={open} setOpen={setOpen} cancelButtonRef={cancelButtonRef}>
+        <TodoModal setOpen={setOpen} cancelButtonRef={cancelButtonRef} />
+      </Modal>
+    </>
   )
 }
---
+++ src/components/pages/todo/components/TodoItem.tsx
@@ -0,0 +1,88 @@
+import { FC, memo, Fragment, SetStateAction } from "react";
+import { Task } from "../../../../types";
+import useStore from "../../../../store/task";
+import { useMutateTask } from "../../../../hooks/useMutateTask";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import { Menu, Transition } from "@headlessui/react";
+
+type TodoItemProps = {
+  task: Omit<Task, 'created_at' | 'updated_at'>
+  setOpen: React.Dispatch<SetStateAction<boolean>>
+}
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const TodoItemMemo: FC<TodoItemProps> = ({
+  task,
+  setOpen,
+}) => {
+  const updateTask = useStore((state) => state.updateEditedTask)
+  const { deleteTaskMutation } = useMutateTask()
+  const handleEdit = (id: number) => {
+    setOpen(true)
+    updateTask({
+      id: id,
+      title: task.title
+    })
+  }
+  return (
+    <li className="flex justify-between items-center gap-x-6 py-4">
+      <div>
+        <p className="text-sm font-semibold leading-6 text-gray-900">{task.title}</p>
+      </div>
+        
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 bg-white py-2">
+            <EllipsisVerticalIcon className="w-6 text-gray-900" />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-20 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={() => handleEdit(task.id)}
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm w-full text-left'
+                    )}
+                  >
+                    Edit
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={() => deleteTaskMutation.mutate(task.id)}
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm w-full text-left'
+                    )}
+                  >
+                    Delete
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </li>
+  )
+}
+
+export const TaskItem = memo(TodoItemMemo)
