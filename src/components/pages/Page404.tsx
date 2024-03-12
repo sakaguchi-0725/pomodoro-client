99743713fbf1c6920f67e363391ff72b43a102fb
sakaguchi <kzmsan63@gmail.com>
2024-03-12 16:43:32 +0900

    404ページ追加

---
+++ src/components/pages/Page404.tsx
@@ -0,0 +1,11 @@
+const Page404 = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <p className="text-gray-600 text-4xl font-semibold">404 | Not Found</p>
+      </div>
+    </div>
+  )
+}
+
+export default Page404
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,14 +4,16 @@ import { Login } from '../components/pages/auth/Login'
 import { MainLayout } from '../components/layout/MainLayout'
 import Main from '../components/pages/main/Main'
 import Report from '../components/pages/report/Report'
+import Page404 from '../components/pages/Page404'
 
 const Router: React.FC = () => {
   return (
     <Routes>
-      <Route path='/signin' element={<Signup />} />
+      <Route path='/signup' element={<Signup />} />
       <Route path='/login' element={<Login />} />
       <Route path="/" element={<MainLayout children={<Main />} />} />
       <Route path="/report" element={<MainLayout children={<Report />} />} />
+      <Route path='*' element={<Page404 />} />
     </Routes>
   )
 }
